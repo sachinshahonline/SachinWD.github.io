3f0f05b04bf6a39da3195533eed877f44a1618f1
Sachin <sachinshahonline16@gmail.com>
2020-08-21 14:11:34 +0530

    forms

--- js/modify.js
+++ js/modify.js
@@ -146,6 +146,7 @@ $(document).ready(function(){
                             return true;
                             
                             
+                            
                         }else{
                             $.alert({
                                 title: 'Alert!',
